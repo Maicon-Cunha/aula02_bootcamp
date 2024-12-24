4e092990462c53ef37d64e362ecd8b3edf771dff
Maicon Cunha <maicoboeira@hotmail.com>
2024-12-24 11:55:30 -0300

    adicionando exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -25,7 +25,7 @@ print(calculo)
 
 #raio_do_circulo = float(input("Digite o raio: "))
 #area_do_circulo = math.pi * raio_do_circulo ** 2
-#print(area_do_circulo)
+#print(f"{area_do_circulo: .2f}")
 
 # #### Strings (`str`)
 
@@ -33,6 +33,13 @@ print(calculo)
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+data = input("Insira uma data no formato dd/mm/aaaa: ")
+lista_dia_mes_ano = data.split("/")
+print(f"O dia é: {lista_dia_mes_ano[0]}")
+print(f"O mes é: {lista_dia_mes_ano[1]}")
+print(f"O ano é: {lista_dia_mes_ano[2]}")
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
