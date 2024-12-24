248691457c12800ecdb5b327a399326ae70e1e17
Maicon Cunha <maicoboeira@hotmail.com>
2024-12-24 19:08:37 -0300

    adicionando exercicio 09

--- exercicios.py
+++ exercicios.py
@@ -52,12 +52,16 @@ import math
 
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 
-base = float(input("Insira a base: "))
-expoente = float(input("Insira o expoente: "))
-resultado = (base)**expoente
-print(f"O resultado de {base} elevado a {expoente} é: {resultado}")
+#base = float(input("Insira a base: "))
+#expoente = float(input("Insira o expoente: "))
+#resultado = (base)**expoente
+#print(f"O resultado de {base} elevado a {expoente} é: {resultado}")
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+celsius = float(input("Insira a temperatura em graus Celsius: "))
+fator = celsius * (9/5) + 32
+print(f"O resultado de Celsius p Fahrenheit é: {fator}")
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
 #raio_do_circulo = float(input("Digite o raio: "))
