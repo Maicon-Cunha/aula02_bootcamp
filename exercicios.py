5934f6bfc468b11a58aed3388f4e59ef4f01882d
Maicon Cunha <maicoboeira@hotmail.com>
2024-12-26 11:07:53 -0300

    adicionando exercicio 15

--- exercicios.py
+++ exercicios.py
@@ -83,8 +83,8 @@ import math
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 
-texto = input("Digite uma frase: ").strip()
-print(texto)
+#texto = input("Digite uma frase: ").strip()
+#print(texto)
 
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
@@ -95,6 +95,10 @@ print(texto)
 #print(f"O ano é: {lista_dia_mes_ano[2]}")
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+palavra01 = input("Insira a primeira palavra: ")
+palavra02 = input("Insira a segunda palavra: ")
+uniao = palavra01 + palavra02
+print(uniao)
 
 # #### Booleanos (`bool`)
 
